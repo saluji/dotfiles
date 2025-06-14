b16973140f0ee88b6d2877586438d18be971ce17
Saluji <erwin_oudomvylay@hotmail.de>
2025-06-14 21:01:26 +0200

    removed old gpu line

--- config/waybar/scripts/gpu.sh
+++ config/waybar/scripts/gpu.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 usage=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | head -n 1)
 echo "$usage"
-
-#!/bin/bash
-#temp=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits | head -n 1)
-#echo "$usage% ${temp}°C"
